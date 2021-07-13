123eb6461c4778ba9f5ca631f73c6a625b5719ae
unknown <fatma_mama@hotmail.fr>
2021-07-13 22:42:26 +0100

    home section

---
+++ main.js
@@ -0,0 +1,8 @@
+ // typing text animation script
+ var typed = new Typed(".typing", {
+    strings: ["Junior full-stack JS Developer"],
+    typeSpeed: 100,
+    backSpeed: 70,
+    loop: true
+});
+
